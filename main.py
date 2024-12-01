137bbc8d5df720daca8ff9488605bc68a4085130
Gardocode <117958690+Gardocode@users.noreply.github.com>
2024-12-01 19:16:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 from infraestructure.mysqlconnection import MySQLConnection
-from infraestructure.users_repository import 
+from infraestructure.user_repository import UserRepository
 from credentials_db import user, password, database, host
-from models.usuarios import 
+from models.user import User
 
 conn = MySQLConnection(host, user, password, database)
-client_repository = ClientRepository(conn)
+user_repository = UserRepository(conn)
 
 while True:
     print("MENU MANTENEDOR")
-    print("1. Mostrar todos los clientes")
-    print("2. Agregar un cliente")
-    print("3. Editar un cliente")
-    print("4. Eliminar un cliente")
+    print("1. Mostrar todos los usuarios")
+    print("2. Agregar un usuario")
+    print("3. Editar un usuario")
+    print("4. Eliminar un usuario")
     print("5. Salir")
     option = input("Ingrese su opcion: ")
     if(option == "1"):
         print("id \t\t nombre \t\t email")
-        clients = client_repository.get_all_client()
-        for client in clients:
-            print(f"{client.get_id()} \t\t {client.get_name()} \t\t {client.get_email()}")
+        users = user_repository.get_all_user()
+        for user in users:
+            print(f"{user.get_id()} \t\t {user.get_name()} \t\t {user.get_email()}")
     if(option == "2"):
-        name = input("Ingrese el nombre del cliente: ")
-        email = input("Ingrese el email del cliente: ")
-        client = Client()
-        client.set_name(name)
-        client.set_email(email)
-        client_repository.create_client(client)
-        print("Cliente registrado exitosamente.")
+        name = input("Ingrese el nombre del usuario: ")
+        email = input("Ingrese el email del usuario: ")
+        user = User()
+        user.set_name(name)
+        user.set_email(email)
+        user_repository.create_user(user)
+        print("Usuario registrado exitosamente.")
     if(option == "3"):
-        id = input("Ingrese el id del cliente: ")
-        name = input("Ingrese el nombre del cliente: ")
-        email = input("Ingrese el email del cliente: ")
-        client = Client()
-        client.set_id(id)
-        client.set_name(name)
-        client.set_email(email)
-        client_repository.update_client(client)
-        print("El cliente fue modificado exitosamente.")
+        id = input("Ingrese el id del usuario: ")
+        name = input("Ingrese el nombre del usuario: ")
+        email = input("Ingrese el email del usuario: ")
+        user = User()
+        user.set_id(id)
+        user.set_name(name)
+        user.set_email(email)
+        user_repository.update_user(user)
+        print("El usuario fue modificado exitosamente.")
     if (option == "4"):
-        id = input("Ingrese el id del cliente: ")
-        client_repository.delete_client(id)
-        print("El cliente fue eliminado exitosamente.")
+        id = input("Ingrese el id del usuario: ")
+        user_repository.delete_user(id)
+        print("El usuario fue eliminado exitosamente.")
     if(option == "5"):
         break
