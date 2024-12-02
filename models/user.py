a2b1a305b20d80cc5502fa7e524f27a27aab6020
nsaav583 <nicolas.saavedra1337@gmail.com>
2024-12-01 21:31:42 -0300

    Update user.py
    modelo clase usuario con get y set de password

--- models/user.py
+++ models/user.py
@@ -1,30 +1,38 @@
+import bcrypt
+
 class User:
     def __init__(self) -> None:
-        self.__id: int = -1
-        self.__name: str = ""
-        self.__email: str = ""
+        self.__usuario: str = ""
         self.__password: str = ""
-    
-    def get_id(self):
-        return self.__id
-    
-    def set_id(self, id: int):
-        self.__id = id
+        self.__id = -1
 
-    def get_name(self) -> str:
-        return self.__name
-    
-    def set_name(self, name: str):
-        self.__name = name
+    def get_usuario(self) -> str:
+        return self.__usuario
 
-    def get_email(self) -> str:
-        return self.__email
-    
-    def set_email(self, email: str):
-        self.__email = email
+    def set_usuario(self, usuario: str):
+        self.__usuario = usuario
 
     def get_password(self) -> str:
         return self.__password
-    
-    def set_password(self, password: str) -> None:
-        self.__password = password
+
+    def set_password(self, password: str):
+        # encripta la contraseña antes de asignarla
+        self.__password = self._hash_password(password)
+
+    def _hash_password(self, password: str) -> str:
+        # Genera el hash de la contraseña usando bcrypt."""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password.decode('utf-8')  # Retorna la contraseña encriptada
+
+    def get_id(self) -> int:
+        return self.__id
+
+    def set_id(self, user_id: int):
+        self.__id = user_id
+
+    def check_password(self, password: str) -> bool:
+        
+        # Verifica si la contraseña proporcionada coincide con la encriptada
+        
+        return bcrypt.checkpw(password.encode('utf-8'), self.__password.encode('utf-8'))
