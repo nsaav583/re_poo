49194db6c4baf092a0a5ec39def281bc054900f6
Gardocode <117958690+Gardocode@users.noreply.github.com>
2024-12-10 14:01:14 -0300

    Update main.py
    Implementación para el prestamo de libros en el menu, falta implementar la devolución de libros, cambio al uso de input para el metodo de update, importación de lo necesario para usar la clase loan(prestamo)

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from infraestructure.mysqlconnection import MySQLConnection
 from infraestructure.user_repository import UserRepository
 from infraestructure.book_repository import BookRepository
+from infraestructure.loan_repository import LoanRepository
 from credentials_db import user, password, database, host
 from models.user import User
-from models.book import Book
 
 conn = MySQLConnection(host, user, password, database)
 user_repository = UserRepository(conn)
 book_repository = BookRepository(conn)
+loan_repository = LoanRepository(conn)
 
 #menu para la gestión o CRUD de libros
 def menu_libros():
-    print("Bienvenido a la tienda")
     
     while True:
         try:
@@ -21,12 +21,12 @@ def menu_libros():
             print("3. Ver información de un libro")
             print("4. Editar un libro")                
             print("5. Eliminar un libro")
-            print("6. Volver al menu principal")
+            print("6. Pedir prestado un libro")
+            print("7. Devolver un libro")
+            print("8. Volver al menu principal")
             option = input("Ingrese una opción: ")
             if option == "1":
-                # opcion refactorizada
-                print("Ingrese a continuación los siguientes datos del libro que desea crear")
-                book = book_repository.get_book_input()
+                book = book_repository.get_book_input() # opcion refactorizada
                 book_repository.create_book(book)
                 print("Libro creado con exito")
             elif option == "2":
@@ -40,17 +40,12 @@ def menu_libros():
                     book_repository.get_book_by_id(id)
             elif option == "4":
                 book_repository.all_books_info()
-                id = int(input("Eliga el ID del libro que desea editar: "))
-                if book_repository.valid_id(id) is False:
-                    print("Debe ingresar un ID valido")
-                else:
                 #ver si se puede optimizar y reutilizar el codigo para hacer preguntas, como se hizo en el semestre anterior
-                    print("Ingrese a continuación los siguientes datos del libro que desea editar")
                 # se utiliza la funcion que pide los datos del libro
-                    book = book_repository.get_book_input()
-                    book_repository.update_book(book)
-                    print("\n")
-                    print("Libro editado correctamente")
+                book = book_repository.get_book_input_for_editing()
+                book_repository.update_book(book)
+                print("\n")
+                print("Libro editado correctamente")
             elif option == "5":
                 book_repository.all_books_info()
                 id = int(input("Ingrese el ID del libro que desea eliminar: "))
@@ -60,6 +55,19 @@ def menu_libros():
                     book_repository.delete_book(id)
                     print("Libro eliminado correctamente")
             elif option == "6":
+                book_repository.all_books_info()
+                id = int(input("Ingrese el ID del libro que desea tomar prestado: "))
+                if book_repository.valid_id(id) is None:
+                    print("Debe ingresar un ID valido")
+                else:
+                    name = input("Ingrese el nombre del usuario: ")
+                    password = input("Ingrese la contraseña del usuario: ")
+                    user = user_repository.login_user(name, password) #desde el login se sacan los datos del usuario
+                    book = book_repository.get_book_by_id(id) #desde este metodo se toman los datos del libro usando el id
+                    loan_repository.loan_book(book, user) #este metodo toma un libro y un usuario para guardar la información   
+            elif option == "7":
+                pass
+            elif option == "8":
                 print("\n")
                 print("Volviendo al menu principal")
                 print("\n")
@@ -94,7 +102,7 @@ while True:
         elif result is None:  # si es None, las credenciales no son válidas
             print("El Usuario o contraseña no son válidos.")
         else:  # si no es un mensaje de error ni none, entonces es un objeto User válido
-            print(f"Bienvenido {result.get_name()}")
+            print(f"Bienvenido a la tienda {result.get_name()}")
             menu_libros()
     elif(option == "3"):
         print("El programa se ha cerrado correctamente")
@@ -102,7 +110,6 @@ while True:
     else:
         print("debe ingresar una opción valida, entre 1 a 3")
 
-
 # ESTO ES PARA VER DETALLE DE UN LIBRO Y ALMACENARLO EN LA BASE DE DATOS (debo implementarlo en menu)
 # isbn = input("Ingrese ISBN del libro")  # ISBN de ejemplo
-# book_repository.obtener_book_desde_api(isbn)
+# book_repository.get_book_from_api(isbn)
