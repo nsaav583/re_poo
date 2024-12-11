2fb36f5244111f50f0be2e03735f8b24d57ffe73
Gardocode <117958690+Gardocode@users.noreply.github.com>
2024-12-11 16:03:58 -0300

    Create logs_utils.py
    creación de la logica para los log y su registro en base de datos

---
+++ infraestructure/logs_utils.py
@@ -0,0 +1,13 @@
+from infraestructure.connection import Connection
+
+class Logger:
+    def __init__(self, conn = Connection) -> None:
+        self.__conn = conn
+
+    def register_log(self, message):
+        try:
+            sql = "INSERT INTO log (message) VALUES (%s)"
+            self.__conn.execute(sql, (message,))
+            self.__conn.commit()
+        except Exception as e:
+            print(f"[ERROR] No se pudo registrar el log: {e}")
